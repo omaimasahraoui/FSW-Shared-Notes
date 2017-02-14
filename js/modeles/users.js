4bd6bc918996ad33d97bf5976846e71e158b6155
Ferid HELALI <Ferid HELALI>
2017-02-14 10:38:52 +0100

    MVC 1.0.1 release is Ok

--- js/modeles/users.js
+++ js/modeles/users.js
@@ -1,10 +1,31 @@
-(function () {
-    users = [];
-    var user = function (id, name, email, mdp, avatar) {
+exports = {};
+
+    var userModel = function (id, name, email, mdp, avatar) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.mdp = mdp;
         this.avatar = avatar;
     }
-    })(jQuery);
+
+    var users = [];
+    var friends = [];
+
+    var salah = new userModel(2, 'salah', 'salah@gmail.com', "", './assets/img2.jpg');
+    var ali = new userModel(1, 'Zin El Abidin Ben ali', 'ali@gmail.com', "", './assets/img1.jpg');
+    var hela = new userModel(3, 'Hela', 'hela@gmail.com', "", './assets/img3.jpg');
+
+    users.push(ali);
+    users.push(salah);
+    users.push(hela);
+    friends.push(ali);
+    friends.push(salah);
+    friends.push(hela);
+
+exports.userModel=userModel;
+exports.users=users;
+exports.friends=friends;
+
+
+
+
